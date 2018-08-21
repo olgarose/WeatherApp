4230833a384d5c2c525c597837ad752bf4fe4ae5
olgarose <olgarose@users.noreply.github.com>
2018-08-20 19:08:46 -0700

    Print JSON to console

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1820%20lombard%20ave',
   json : true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
