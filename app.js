f1a5ef36d9b5573c9748e045450de9d221731e1b
olgarose <olgarose@users.noreply.github.com>
2018-08-21 18:16:33 -0700

    Take out geocode from app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const request = require('request');
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
@@ -17,20 +16,3 @@ const argv = yargs
   .argv;
 
 geocode.geocodeAddress(argv.address);
-
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url : `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json : true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  }
-});
