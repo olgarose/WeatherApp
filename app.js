181a53d27352bb5fe4a0e45812f4ead03cb82b8e
olgarose <olgarose@users.noreply.github.com>
2018-08-22 14:32:04 -0700

    Hooking up weather API

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
 // geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 //   if (errorMessage) {
 //     console.log(errorMessage);
@@ -22,18 +23,12 @@
 //     console.log(JSON.stringify(results, undefined, 2));
 //   }
 // });
-// 6a3ccd3c578b08aafc4fe043f1d5e96c
 
-const request = require('request');
-var apiKey = '6a3ccd3c578b08aafc4fe043f1d5e96c'
-
-request({
-  url : `https://api.darksky.net/forecast/${apiKey}/47.6338217,-122.3215448`,
-  json : true
- }, (error, response, body) => {
-   if (!error && response.statusCode === 200) {
-      console.log(body.currently.temperature);
-   } else {
-     console.log('Unable to fetch weather.');
-   }
- });
+// lat, lng, callback (err, results)
+weather.getWeather(47.6338217, -122.3215448, (errorMessage, weatherResults) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(weatherResults, undefined, 2));
+  }
+});
